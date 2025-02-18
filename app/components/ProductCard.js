4b7bbefaac63a8864627410cd9f18c64da31c670
Alexmaxwell <Alexmaxwell3096@gmail.com>
2025-02-19 00:27:04 +0530

    fix: footer component

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,10 +1,19 @@
 export default function ProductCard({ product }) {
-    return (
-      <div className="border p-4 rounded-lg shadow-md">
-        <img src={product.thumbnail} alt={product.title} className="w-full h-40 object-cover rounded-md" />
-        <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
-        <p className="text-gray-600">${product.price}</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex flex-1 flex-col p-8">
+      <img alt="" src={product.thumbnail} className="mx-auto h-40 shrink-0 rounded-md" />
+      <h2 className="mt-6 text-lg font-medium">{product.name}</h2>
+      <dl className="mt-1 flex grow flex-col justify-between">
+        <dt className="sr-only">Title</dt>
+        <dd className="text-lg font-semibold mb-3">{product.title}</dd>
+        <dd className="text-sm font-semibold text-gray-500">{product.description}</dd>
+        <dt className="sr-only">Price</dt>
+        <dd className="mt-3">
+          <span className="inline-flex items-center px-2 py-1 text-lg font-semibold">
+            ${product.price}
+          </span>
+        </dd>
+      </dl>
+    </div>
+  );
+}
--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -6,10 +6,12 @@ export default function ProductList() {
   const { filteredProducts, loading } = useProductStore();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4" >
       {filteredProducts.length > 0 ? (
         filteredProducts.map((product) => (
+          <div className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white shadow">
           <ProductCard key={product.id} product={product} />
+          </div>
         ))
       ) : (
         <p className="text-center col-span-3 text-gray-500">
